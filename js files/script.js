648de786c6ba76f110dda0f2f7440cc3783d376f
tahahasan7 <abdella.boutaarourt@student.ehb.be>
2023-01-31 14:41:37 +0100

    font and hamburger

--- js files/script.js
+++ js files/script.js
@@ -1,6 +1,29 @@
 "use strict"
 
 
-document.getElementsByClassName('burger1').addEventListener(onclick, () =>{
-    
+
+function button(){
+document.getElementById('open-menu').addEventListener('click', () => {
+    document.getElementById('burger2').style.display = "flex"
+
+    document.getElementById('close-menu').style.display = "block"
+    document.getElementById('open-menu').style.display = "none"
+
+   
 })
+
+document.getElementById('close-menu').addEventListener('click', () => {
+    document.getElementById('burger2').style.display = "none"
+
+    document.getElementById('close-menu').style.display = "none"
+    document.getElementById('open-menu').style.display = "block"
+
+   
+})
+}
+
+button()
+
+
+
+
